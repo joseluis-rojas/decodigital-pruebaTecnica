952b75cd80dcae76d09354e0d1bdddd465eede11
joseluis-rojas <56558839+joseluis-rojas@users.noreply.github.com>
2023-07-05 13:36:20 -0400

    codigo fuente

---
+++ Prueba Practica/Prueba Practica/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Prueba_Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.txtTextoInvertido = new System.Windows.Forms.TextBox();
+            this.btnInvertir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnEvaluar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbColores = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.btnIf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(26, 85);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(211, 20);
+            this.txtTexto.TabIndex = 0;
+            // 
+            // txtTextoInvertido
+            // 
+            this.txtTextoInvertido.Location = new System.Drawing.Point(26, 137);
+            this.txtTextoInvertido.Name = "txtTextoInvertido";
+            this.txtTextoInvertido.ReadOnly = true;
+            this.txtTextoInvertido.Size = new System.Drawing.Size(211, 20);
+            this.txtTextoInvertido.TabIndex = 1;
+            // 
+            // btnInvertir
+            // 
+            this.btnInvertir.Location = new System.Drawing.Point(92, 173);
+            this.btnInvertir.Name = "btnInvertir";
+            this.btnInvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnInvertir.TabIndex = 2;
+            this.btnInvertir.Text = "invertir";
+            this.btnInvertir.UseVisualStyleBackColor = true;
+            this.btnInvertir.Click += new System.EventHandler(this.btnInvertir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingresar texto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado inverido";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(264, 85);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(211, 20);
+            this.txtNumero.TabIndex = 5;
+            // 
+            // btnEvaluar
+            // 
+            this.btnEvaluar.Location = new System.Drawing.Point(329, 173);
+            this.btnEvaluar.Name = "btnEvaluar";
+            this.btnEvaluar.Size = new System.Drawing.Size(75, 23);
+            this.btnEvaluar.TabIndex = 6;
+            this.btnEvaluar.Text = "Evaluar";
+            this.btnEvaluar.UseVisualStyleBackColor = true;
+            this.btnEvaluar.Click += new System.EventHandler(this.btnEvaluar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingresar numero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "INVERTIR TEXTO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(259, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "NUMEROS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(498, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "COLORES";
+            // 
+            // cmbColores
+            // 
+            this.cmbColores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColores.FormattingEnabled = true;
+            this.cmbColores.Items.AddRange(new object[] {
+            "Amarillo",
+            "Morado",
+            "Rosa",
+            "Azul",
+            "Blanco"});
+            this.cmbColores.Location = new System.Drawing.Point(503, 85);
+            this.cmbColores.Name = "cmbColores";
+            this.cmbColores.Size = new System.Drawing.Size(211, 21);
+            this.cmbColores.TabIndex = 11;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(503, 137);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(211, 112);
+            this.txtResultado.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(500, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Seleccione color";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(500, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "tabla seleccuionada";
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(735, 85);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 15;
+            this.btnSwitch.Text = "SWITCH";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // btnIf
+            // 
+            this.btnIf.Location = new System.Drawing.Point(816, 85);
+            this.btnIf.Name = "btnIf";
+            this.btnIf.Size = new System.Drawing.Size(75, 23);
+            this.btnIf.TabIndex = 16;
+            this.btnIf.Text = "IF";
+            this.btnIf.UseVisualStyleBackColor = true;
+            this.btnIf.Click += new System.EventHandler(this.btnIf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 261);
+            this.Controls.Add(this.btnIf);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.cmbColores);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEvaluar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInvertir);
+            this.Controls.Add(this.txtTextoInvertido);
+            this.Controls.Add(this.txtTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.TextBox txtTextoInvertido;
+        private System.Windows.Forms.Button btnInvertir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnEvaluar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbColores;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Button btnIf;
+    }
+}
+
---
+++ Prueba Practica/Prueba Practica/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_Practica
+{
+   
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static string InvertirTexto(string texto)
+        {
+            char[] caracteres = texto.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+
+        static void EvaluarParidad(int numero)
+        {
+            if (numero % 2 == 0)
+            {
+                MessageBox.Show("El número " + numero + " es par");
+            }
+            else
+            {
+                MessageBox.Show("El número " + numero + " es impar");
+            }
+        }
+        private void btnInvertir_Click(object sender, EventArgs e)
+        {
+            string texto = txtTexto.Text;
+            string textoInvertido = InvertirTexto(texto);
+            txtTextoInvertido.Text = textoInvertido;
+        }
+        public void GenerarTabla(int numero)
+        {
+            txtResultado.Clear();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                txtResultado.AppendText(numero + "x" + i + " = " + resultado + Environment.NewLine);
+            }
+        }
+
+        private void btnEvaluar_Click(object sender, EventArgs e)
+        {
+            int numero = 0;
+            if (int.TryParse(txtNumero.Text, out numero))
+            {
+                EvaluarParidad(numero);
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido");
+            }
+        }
+
+      
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            string colorSeleccionado = cmbColores.SelectedItem as string;
+
+            if (colorSeleccionado != null)
+            {
+                switch (colorSeleccionado)
+                {
+                    case "Amarillo":
+                        GenerarTabla(2);
+                        break;
+                    case "Morado":
+                        GenerarTabla(7);
+                        break;
+                    case "Rosa":
+                        GenerarTabla(9);
+                        break;
+                    case "Azul":
+                        GenerarTabla(6);
+                        break;
+                    case "Blanco":
+                        GenerarTabla(12);
+                        break;
+                }
+            }
+        }
+
+        private void btnIf_Click(object sender, EventArgs e)
+        {
+            string colorSeleccionado = cmbColores.SelectedItem as string;
+
+            if (colorSeleccionado != null)
+            {
+                if (colorSeleccionado == "Amarillo")
+                {
+                    GenerarTabla(2);
+                }
+                else if (colorSeleccionado == "Morado")
+                {
+                    GenerarTabla(7);
+                }
+                else if (colorSeleccionado == "Rosa")
+                {
+                    GenerarTabla(9);
+                }
+                else if (colorSeleccionado == "Azul")
+                {
+                    GenerarTabla(6);
+                }
+                else if (colorSeleccionado == "Blanco")
+                {
+                    GenerarTabla(12);
+                }
+            }
+        }
+    }
+}
